6e3bd2eef2e40088e188e5acefcdd1f7c0887af4
Angramme <kacpero376@gmail.com>
2023-01-21 18:04:49 +0100

    added hash pair

---
+++ src/cpp_utils.cpp
@@ -0,0 +1,10 @@
+struct pairhash {
+  template <typename T, typename U>
+  std::size_t operator()(const std::pair<T, U> &x) const {
+    // return std::hash<T>()(x.first) ^ std::hash<U>()(x.second); // simpler alternative
+    const auto h1 = hash<T1>{}(p.first);
+    const auto h2 = hash<T2>{}(p.second);
+    if (h1 == h2) return h1;
+    return h1 ^ h2;
+  }
+};
