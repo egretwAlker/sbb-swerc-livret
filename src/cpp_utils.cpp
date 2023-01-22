acc6a971c3a60b8792092c1effe32b6f82461ee8
Angramme <kacpero376@gmail.com>
2023-01-22 23:52:07 +0100

    corrigé une erreur

--- src/cpp_utils.cpp
+++ src/cpp_utils.cpp
@@ -2,10 +2,10 @@
 
 struct pairhash {
   template <typename T, typename U>
-  std::size_t operator()(const std::pair<T, U> &x) const {
+  std::size_t operator()(const std::pair<T, U> &p) const {
     // return std::hash<T>()(x.first) ^ std::hash<U>()(x.second); // simpler alternative
-    const auto h1 = hash<T1>{}(p.first);
-    const auto h2 = hash<T2>{}(p.second);
+    const auto h1 = hash<T>{}(p.first);
+    const auto h2 = hash<U>{}(p.second);
     if (h1 == h2) return h1;
     return h1 ^ h2;
   }
