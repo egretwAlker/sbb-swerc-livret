df86e013d12288277beaec323bec3b456389b6c3
Tianxiang Xia <egrr353@protonmail.com>
2023-02-14 20:17:17 +0100

    presque final

--- src/cpp_utils.cpp
+++ src/cpp_utils.cpp
@@ -77,34 +77,6 @@ template<class I> vi lis(const vector<I>& S) {
 	while (L--) ans[L] = cur, cur = prev[cur];
 	return ans;
 }
-// IntervalContainer.h
-/** * Description: Add and remove intervals from a set of disjoint intervals.
- * Will merge the added interval with any overlapping intervals in the set when adding.
- * Intervals are [inclusive, exclusive).
- * Time: O(\log N) */
-#pragma once
-
-set<pii>::iterator addInterval(set<pii>& is, int L, int R) {
-	if (L == R) return is.end();
-	auto it = is.lower_bound({L, R}), before = it;
-	while (it != is.end() && it->first <= R) {
-		R = max(R, it->second);
-		before = it = is.erase(it);
-	}
-	if (it != is.begin() && (--it)->second >= L) {
-		L = min(L, it->first);
-		R = max(R, it->second);
-		is.erase(it);
-	}
-	return is.insert(before, {L,R});
-}
-
-void removeInterval(set<pii>& is, int L, int R) {
-	if (L == R) return;
-	auto it = addInterval(is, L, R);
-	auto r2 = it->second;
-	if (it->first == L) is.erase(it);
-	else (int&)it->second = L;
 // ConstantIntervals.h
 /** * Description: Split a monotone function on [from, to) into a minimal set of half-open intervals on which it has the same value.
  *  Runs a callback g for each such interval.
